d38d7dfee28fe9d67e5a63cee0dfcec0578fede4
shresthamajumdar08-sudo <shresthamajumdar08@gmail.com>
2026-03-14 21:41:43 +0530

    Add files via upload

---
+++ fristprogram.py
@@ -0,0 +1,57 @@
+print("shrestha")
+name = "shrestha " 
+age  = 20
+age2 = age
+print("my name is ", name)
+print("my age :",age2)
+print(type(name))
+print(type(age2))
+old=False
+print(type(old))
+a=255
+b=56
+a+b
+print(sum)
+
+a=20
+b=20
+result= a+b
+print(result)
+print(20+20)
+a=100
+b=100
+result=a+b
+print(result)
+print(800+5400)
+print(5895612-587968456)
+print(800-800)
+a=1000
+b=500
+diff=a-b
+print(diff)
+a=20
+b=20
+sum=a+b
+print(sum)
+# creating variabls
+name= "Shrestha majumdar"
+age= 20
+address= "Jyangra"
+school= "BMGS"
+print("Name:", name)
+print("Age:",age)
+print("Address:",address)
+print("school:",school)
+print("shrestha")
+print(20)
+age=20
+print("Age:",age)
+#conditions
+marks=60
+if marks>50:
+    print("pass")
+maks =40
+if marks>50:
+    print("pass")
+else:
+    print (False)
