0b08d4a2adf59571eda79000d55d4fbfc4e87c08
Shrestha Majumdar <shresthamajumdar08@gmail.com>
2026-03-16 21:28:44 +0530

    First Python practice program

---
+++ fristprogram.py
@@ -0,0 +1,124 @@
+print("shrestha")
+name = "shrestha " 
+age  = 20
+age2 = age
+print("my name is ", name)
+print("my age :",age2)
+print(type(name))
+print(type(age2))
+old=False
+print(type(old))
+a=255
+b=56
+a+b
+print(sum)
+
+a=20
+b=20
+result= a+b
+print(result)
+print(20+20)
+a=100
+b=100
+result=a+b
+print(result)
+print(800+5400)
+print(5895612-587968456)
+print(800-800)
+a=1000
+b=500
+diff=a-b
+print(diff)
+a=20
+b=20
+sum=a+b
+print(sum)
+# creating variabls
+name= "Shrestha majumdar"
+age= 20
+address= "Jyangra"
+school= "BMGS"
+print("Name:", name)
+print("Age:",age)
+print("Address:",address)
+print("school:",school)
+print("shrestha")
+print(20)
+age=20
+print("Age:",age)
+#conditions
+marks=60
+if marks>50:
+    print("pass")
+maks =40
+if marks>50:
+    print("pass")
+else:
+    print (False)
+marks=75
+if marks>=80:
+    print("Grade A")
+elif marks>=40: 
+    print("Grade B")
+else:
+    print("Grade C")
+marks=45
+if marks>=45:
+    print("pass")
+else:
+    print("fail")
+num=10
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+num=15
+if num % 2==0:
+    print("even")
+else :
+    print("odd")
+
+# check pass fail
+marks = 50
+if marks >=50:
+    print("pass")
+else:
+    print("fail")
+
+# check even or odd number
+num= 3
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+# Write a program to check whether a person is Adult or Minor.
+age=20
+if age>= 20:
+    print ("Adult")
+else:
+    print("Minor")
+
+# check the biggest number.
+a = 10
+b= 15
+c = 20
+if a<b<c:
+    print ("c is biggest number")
+elif a>b>c:
+    print("a is biggest number")
+else :
+    print("b is the biggest number")
+
+
+if a>b and a>c:
+    print("a is bigger number")
+elif a<b and c<b:
+    print("b is bigger number")
+else :
+    print("c is the biggeer number")
+
+
+
+ 
