03972ac44542d9858ecf51a2edb86f428c0026e6
Shrestha Majumdar <shresthamajumdar08@gmail.com>
2026-03-18 21:37:05 +0530

    re-upload project

---
+++ fristprogram.py
@@ -0,0 +1,294 @@
+print("shrestha")
+name = "shrestha " 
+age  = 20
+age2 = age
+print("my name is ", name)
+print("my age :",age2)
+print(type(name))
+print(type(age2))
+old=False
+print(type(old))
+a=255
+b=56
+a+b
+print(sum)
+
+a=20
+b=20
+result= a+b
+print(result)
+print(20+20)
+a=100
+b=100
+result=a+b
+print(result)
+print(800+5400)
+print(5895612-587968456)
+print(800-800)
+a=1000
+b=500
+diff=a-b
+print(diff)
+a=20
+b=20
+sum=a+b
+print(sum)
+# creating variabls
+name= "Shrestha majumdar"
+age= 20
+address= "Jyangra"
+school= "BMGS"
+print("Name:", name)
+print("Age:",age)
+print("Address:",address)
+print("school:",school)
+print("shrestha")
+print(20)
+age=20
+print("Age:",age)
+#conditions
+marks=60
+if marks>50:
+    print("pass")
+maks =40
+if marks>50:
+    print("pass")
+else:
+    print (False)
+marks=75
+if marks>=80:
+    print("Grade A")
+elif marks>=40: 
+    print("Grade B")
+else:
+    print("Grade C")
+marks=45
+if marks>=45:
+    print("pass")
+else:
+    print("fail")
+num=10
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+num=15
+if num % 2==0:
+    print("even")
+else :
+    print("odd")
+
+# check pass fail
+marks = 50
+if marks >=50:
+    print("pass")
+else:
+    print("fail")
+
+# check even or odd number
+num= 3
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+# Write a program to check whether a person is Adult or Minor.
+age=20
+if age>= 20:
+    print ("Adult")
+else:
+    print("Minor")
+
+# check the biggest number.
+a = 10
+b= 15
+c = 20
+if a<b<c:
+    print ("c is biggest number")
+elif a>b>c:
+    print("a is biggest number")
+else :
+    print("b is the biggest number")
+
+# 15th March 2026
+
+if a>b and a>c:
+    print("a is bigger number")
+elif a<b and c<b:
+    print("b is bigger number")
+else :
+    print("c is the biggeer number")
+a=70
+if 90>=a:
+    print("GRADE A")
+elif 70>=a:
+    print("GRADE B")
+else:
+    print("GRADE C") 
+
+if marks>=90:
+    print("Grade A")
+if marks>=70:
+    print("Grade B")
+if marks >=40:
+    print("Grade C")
+if marks <40:
+    print("Fail")
+
+
+num = 4
+if num % 2== 0 :
+    print ("even")
+else:
+    print(" odd")
+num1=15
+num2 =25
+num3=35
+if num1>num2 and num1 >num3:
+    print("lager number 1")
+elif num2>num1 and num2>num3:
+    print("lager number 2")
+else:
+    print("lager number 3")
+marks = 50
+if marks >=45:
+    print("pass")
+else:
+    print("fail")
+
+#largest tow number.......
+marks1 = 100
+marks2= 900
+if marks1 > marks2:
+    print("Marks 1 is largest number")
+else:
+    print("Marks 2 is largest number")
+
+#largest three number...............
+marks1=100
+marks2=800
+marks3=900
+if marks1>marks2 and marks1>marks3:
+    print("Marks 1 is largest number")
+elif marks2>marks1 and marks2>marks3:
+    print("Marks 2 is largest number")
+else:
+    print("Marks 3 is largest number")
+#Assign Grade Based om Marks...................
+marks=75
+if marks>=80:
+    print ("Grade A")
+elif marks>=60:
+    print("Grade B")
+elif marks>=40:
+    print ("Grade C")
+else:
+    print("Fail")
+
+age=20
+if age>= 18:
+     print("eligible for vote")
+elif age<= 16:
+    print("not eligible foe vote")
+
+year=2024
+if year %4 == 0 :
+    print(" its leap year")
+else:
+    print("it's not leap year")
+
+salary = 10000000 
+if salary >=100000:
+    print("20% bonus")
+elif salary >=500000:
+    print("10% bonus")
+elif salary >= 300000:
+    print(" 5% bonus")
+else :
+    print("no bonus")
+
+
+# Number Divisibility Test
+
+num=18
+if num % 2 ==0 and num % 3== 0 :
+    print("Even and divisible by 3")
+elif num % 2 == 0:
+    print("Even only")
+elif num % 3 == 0 :
+    print("Only divisible by 3")
+else:
+    print("Nither")
+
+# 16 March 2026
+num1= 200
+num2= 800
+if num1> num2 :
+    print("200 is greatest number")
+else :
+    print("800 is greatest number")
+
+age=20
+if age >=18 :
+    print("you are eligible for vote")
+    print("you are 18 year old")
+else:
+    print(" you are not eligible for vote")
+
+print(10>5 and 45>40)
+
+# number range check..............
+x= 25
+if x>=30 and 20>=x:
+    print ("25 number b/w to 20 and 30")
+else:
+    print("the number is not in range")
+
+x=25
+if 20 <= x <= 30:
+    print("25 number b/w to 20 and 30")
+else:
+    print("the number is not in range")
+
+num=15
+if 10<=num<=20:
+    print("15 number b/w 10 to 20 ")
+else:
+    print("out of range")
+
+num=120
+if 1<= num >= 100:
+    print("valid number")
+else:
+    print("invalid number")
+
+temp=32
+if temp >= 40:
+    print("very hot")
+elif temp>= 30:
+    print("hot")
+elif temp>= 20:
+    print("warm")
+else:
+    print("cold")
+
+age=15
+if age >=60:
+    print("senior")
+elif age >=18:
+    print("adult")
+elif age>= 13:
+    print("teen")
+else:
+    print("child")
+
+marks = 75
+if marks>=90:
+    print("grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks>= 40:
+    print("grade C")
+else:
+    print("grade c")
