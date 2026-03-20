b1b0cac3a08b0c0e842fb65d73d98bc633104bcc
Shrestha Majumdar <shresthamajumdar08@gmail.com>
2026-03-21 00:52:47 +0530

    update 127 lines

--- fristprogram.py
+++ fristprogram.py
@@ -1,357 +1,129 @@
 print("shrestha")
-name = "shrestha " 
-age  = 20
-age2 = age
-print("my name is ", name)
-print("my age :",age2)
-print(type(name))
-print(type(age2))
-old=False
-print(type(old))
-a=255
-b=56
-a+b
-print(sum)
-
 a=20
+b=50
+print(a+b)
+print(a*b)
+print(a//b)
+print(a/b)
+x=10
+x+=5
+print(x)
+age=20
+if age >=18 and age<=25:
+    print("young")
+
+
+num=[10,20,90]
+name=["Rahul","Shrestha","rini"]
+print(name[-1])
+print(num[2])
+print(len(name))
+
+
+x= 2
+while x <=10:
+    print(x)
+    x += 1
+
+print("the terminal has been clearned")
+
+i=1
+while i<=5:
+    print(i)
+    i+=1
+
+s=2
+while s<=10:
+    print(s)
+    s+=2
+
+s=10
+while s>=1:
+    print(s)
+    s-=1
+
+
+#Take a character and check whether it is vowel or consonant....................
+charecter = input("Enter a character: ")
+if charecter ("a","e","i","o","u"):
+    print("vowel")
+else:
+    print("consonant")
+
+
+
+# Take two numbers and an operator (+ - * /) and create a simple calculator using if–elif............................
+a=10
 b=20
-result= a+b
-print(result)
-print(20+20)
-a=100
-b=100
-result=a+b
-print(result)
-print(800+5400)
-print(5895612-587968456)
-print(800-800)
-a=1000
-b=500
-diff=a-b
-print(diff)
-a=20
-b=20
-sum=a+b
-print(sum)
-# creating variabls
-name= "Shrestha majumdar"
-age= 20
-address= "Jyangra"
-school= "BMGS"
-print("Name:", name)
-print("Age:",age)
-print("Address:",address)
-print("school:",school)
-print("shrestha")
-print(20)
-age=20
-print("Age:",age)
-#conditions
-marks=60
-if marks>50:
-    print("pass")
-maks =40
-if marks>50:
-    print("pass")
+if (a+b)==30:
+    print("sum")
+elif(a-b)==30:
+    print("subtraction")
+elif(a*b)==30:
+    print("multiplication")
 else:
-    print (False)
-marks=75
-if marks>=80:
-    print("Grade A")
-elif marks>=40: 
-    print("Grade B")
+    print("devision")
+
+
+# Take a number and check if it is multiple of 5............................
+num=int(input("Enter a Number:  "))
+if num%5==0:
+    print("it can divided by 5")
 else:
-    print("Grade C")
-marks=45
-if marks>=45:
-    print("pass")
+    print("It is not divided by 5")
+
+
+# Take a string and check whether its length is greater than 5 or not............................
+s= input("Enter a string value:  ")
+if (len(s))>=5:
+    print("lengh is greater than 5")
 else:
-    print("fail")
-num=10
-if num % 2 == 0:
-    print("even")
+    print("lengh is not greater than 5")
+
+
+#Take two strings and check whether they are equal or not......................
+s1=input("first string value: ")
+s2=input("second string value : ")
+if s1==s2:
+    print("first & second string values are equal.")
 else:
-    print("odd")
+    print("first & second string value are not equal.")
 
-num=15
-if num % 2==0:
-    print("even")
-else :
-    print("odd")
 
-# check pass fail
-marks = 50
-if marks >=50:
-    print("pass")
+#Take a letter and check:.....................
+#Uppercase.........
+#Lowercase.........
+#Not an alphabet.....
+
+l=input("take a letter - ")
+if 
+
+
+
+
+
+#Take number of items purchased and print discount:
+#1–5 → No Discount
+#6–10 → 10% Discount
+#Above 10 → 20% Discount
+
+item= int(input("Take number of items -  "))
+if item>=10:
+    print("20% Discount")
+elif item>=6:
+    print("10% Discount")
 else:
-    print("fail")
+    print("No Discount")
 
-# check even or odd number
-num= 3
-if num % 2 == 0:
-    print("even")
+
+# Take a string and check whether the first letter is a vowel......................
+s=input("Take a string valve- ")
+if s [0] ("a","e","i","o","u"):
+    print("vowel")
 else:
-    print("odd")
+    print("consonant")
 
-# Write a program to check whether a person is Adult or Minor.
-age=20
-if age>= 20:
-    print ("Adult")
-else:
-    print("Minor")
-
-# check the biggest number.
-a = 10
-b= 15
-c = 20
-if a<b<c:
-    print ("c is biggest number")
-elif a>b>c:
-    print("a is biggest number")
-else :
-    print("b is the biggest number")
-
-# 15th March 2026
-
-if a>b and a>c:
-    print("a is bigger number")
-elif a<b and c<b:
-    print("b is bigger number")
-else :
-    print("c is the biggeer number")
-a=70
-if 90>=a:
-    print("GRADE A")
-elif 70>=a:
-    print("GRADE B")
-else:
-    print("GRADE C") 
-
-if marks>=90:
-    print("Grade A")
-if marks>=70:
-    print("Grade B")
-if marks >=40:
-    print("Grade C")
-if marks <40:
-    print("Fail")
+#............................................................................................................................................
 
 
-num = 4
-if num % 2== 0 :
-    print ("even")
-else:
-    print(" odd")
-num1=15
-num2 =25
-num3=35
-if num1>num2 and num1 >num3:
-    print("lager number 1")
-elif num2>num1 and num2>num3:
-    print("lager number 2")
-else:
-    print("lager number 3")
-marks = 50
-if marks >=45:
-    print("pass")
-else:
-    print("fail")
-
-#largest tow number.......
-marks1 = 100
-marks2= 900
-if marks1 > marks2:
-    print("Marks 1 is largest number")
-else:
-    print("Marks 2 is largest number")
-
-#largest three number...............
-marks1=100
-marks2=800
-marks3=900
-if marks1>marks2 and marks1>marks3:
-    print("Marks 1 is largest number")
-elif marks2>marks1 and marks2>marks3:
-    print("Marks 2 is largest number")
-else:
-    print("Marks 3 is largest number")
-#Assign Grade Based om Marks...................
-marks=75
-if marks>=80:
-    print ("Grade A")
-elif marks>=60:
-    print("Grade B")
-elif marks>=40:
-    print ("Grade C")
-else:
-    print("Fail")
-
-age=20
-if age>= 18:
-     print("eligible for vote")
-elif age<= 16:
-    print("not eligible foe vote")
-
-year=2024
-if year %4 == 0 :
-    print(" its leap year")
-else:
-    print("it's not leap year")
-
-salary = 10000000 
-if salary >=100000:
-    print("20% bonus")
-elif salary >=500000:
-    print("10% bonus")
-elif salary >= 300000:
-    print(" 5% bonus")
-else :
-    print("no bonus")
-
-
-# Number Divisibility Test
-
-num=18
-if num % 2 ==0 and num % 3== 0 :
-    print("Even and divisible by 3")
-elif num % 2 == 0:
-    print("Even only")
-elif num % 3 == 0 :
-    print("Only divisible by 3")
-else:
-    print("Nither")
-
-# 16 March 2026
-num1= 200
-num2= 800
-if num1> num2 :
-    print("200 is greatest number")
-else :
-    print("800 is greatest number")
-
-age=20
-if age >=18 :
-    print("you are eligible for vote")
-    print("you are 18 year old")
-else:
-    print(" you are not eligible for vote")
-
-print(10>5 and 45>40)
-
-# number range check..............
-x= 25
-if x>=30 and 20>=x:
-    print ("25 number b/w to 20 and 30")
-else:
-    print("the number is not in range")
-
-x=25
-if 20 <= x <= 30:
-    print("25 number b/w to 20 and 30")
-else:
-    print("the number is not in range")
-
-num=15
-if 10<=num<=20:
-    print("15 number b/w 10 to 20 ")
-else:
-    print("out of range")
-
-num=120
-if 1<= num >= 100:
-    print("valid number")
-else:
-    print("invalid number")
-
-temp=32
-if temp >= 40:
-    print("very hot")
-elif temp>= 30:
-    print("hot")
-elif temp>= 20:
-    print("warm")
-else:
-    print("cold")
-
-age=15
-if age >=60:
-    print("senior")
-elif age >=18:
-    print("adult")
-elif age>= 13:
-    print("teen")
-else:
-    print("child")
-
-marks = 75
-if marks>=90:
-    print("grade A")
-elif marks >= 70:
-    print("Grade B")
-elif marks>= 40:
-    print("grade C")
-else:
-    print("grade c")
-
-
-#  4. Take three numbers and print the smallest number.......................
-num1=20
-num2=30
-num3= 10
-if num1>num2 and num1>num3:
-    print("num1 is smallest number.")
-elif num2>num1 and num2<num3:
-    print("num2 is smallest number. ")
-else:
-    print("num3 is smallest number.")
-
-#  7. Take age as input and print:......................
-age= int(input("Write youe Age : "))
-if age >= 60:
-    print("Senior Citizen")
-elif age>= 20:
-    print("Adult")
-elif age>= 13:
-    print(" Teenager")
-else:
-    print("child")
- 
- # 9. Take a string name and check:
-name= input("Write the Name :")
-if name== "Admin":
-    print("Welcome Admin")
-else:
-    print("Access Denied")
-
-# 11. Take temperature as input and print:
-
-temp =int(input("Write the Temperature :  "))
-if temp>=35:
-    print(" Hot")
-elif temp>=20:
-    print("Warm")
-else:
-    print("cold")
-
- # 12. Take a password string.
-
-password = input("Write the correct Password :  ")
-if password== "python123":
-    print(" Login Successful")
-else:
-    print(" Wrong Password")
-
-
-
-
-# 15. Take a username.
-
-username1 = input("write usename : ")
-username2 = input ("write username : ")
-if username1 ==  "student":
-    print(" Welcome Student")
-elif username2 == "Teacher":
-    print("welcome Teacher")
-else:
-    print("Unknown User")
-
+    
